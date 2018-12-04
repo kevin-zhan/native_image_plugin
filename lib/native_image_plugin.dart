1b8ed2627f7f3543c2f1f17de9a8c5269ab17b71
kevinzhan(湛杨梦晓) <kevinzhan@tencent.com>
2018-12-04 20:46:36 +0800

    add doc

--- lib/native_image_plugin.dart
+++ lib/native_image_plugin.dart
@@ -3,10 +3,18 @@ import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 
+/// A plugin to fetch image data from platform side
+///
+/// You can use it to get image data in `Uint8List` format. And
+/// present it through `Image.memory()`
 class NativeImagePlugin {
+  /// the channel to pass image name and receive image data
   static const MethodChannel _channel =
       const MethodChannel('native_image_plugin');
 
+  /// Get image data from platform resource.
+  ///
+  /// @param imageName The name of the native image resource
   static Future<Uint8List> getNativeImage(String imageName) async {
     var imageData =
         await _channel.invokeMethod("getNativeImage", {"image_name": imageName});
